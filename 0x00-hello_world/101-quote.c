1ad43d28d527ea1aacb121d1e8fe7dc63a1d12ea
Omoyemhen <jessicasamuel.om@gmail.com>
2022-09-08 20:52:27 -0700

    coding

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <unistd.h>
+#include <stdio.h>
 
 /**
 * main - prints a test
 *
 *Return: Always 1 (success)
 */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1)
+}
