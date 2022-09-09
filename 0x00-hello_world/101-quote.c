6f6a104e69ac2b496ca721c97c1c79dadd48c3c4
Omoyemhen <jessicasamuel.om@gmail.com>
2022-09-08 19:59:32 -0700

    coding

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,6 @@
+#include <unistd.h>
+#include <stdio.h>
+
 /**
  * main - print quote
  *
@@ -8,7 +11,6 @@
 
 int main(void
 {
-	write(2, and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59)
-;
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
