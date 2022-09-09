e283842472840b183f3b52e390d27875c08ca1be
Omoyemhen <jessicasamuel.om@gmail.com>
2022-09-08 19:48:18 -0700

    coding

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,12 @@
  *
  * Description: prints Dora's quote
  * 
- * Return: Always 1 (sucess)
+ * Return: Always 1 (Success)
  */
 
-int main(void)
-	write(2, and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+int main(void
+{
+	write(2, and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59)
+;
 	return (1);
+}
