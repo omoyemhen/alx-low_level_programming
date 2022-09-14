00c08e8c1d6d34d0746da167b6b6212e4fb0e8a1
Omoyemhen <jessicasamuel.om@gmail.com>
2022-09-14 16:19:49 -0700

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
 #include "main.h"
+
 /**
- * main - Entry point
- * Description: prints 'main.h \n'
- * Return: Always 0 (success)
+ * main - print putchar
+ * Return: always 0
  */
+
 int main(void)
 {
-char str[] = "main.h\n";
-int i = 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-while (str[i] != '\0')
-       {
-       char c = str[i];
+	return (0);
 
-       _putchar(c);
-       i++;
-       }
-return (0);
 }
