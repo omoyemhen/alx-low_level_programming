df5d045db363ece3b0a5accc26026c4a00da7de4
Omoyemhen <jessicasamuel.om@gmail.com>
2022-09-15 18:46:40 -0700

    nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,6 +27,12 @@ void print_times_table(int n)
 			}
 			else if (rep < 100)
 			{
+				_putchar(' ');
+				_putchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
+			}
+			else
+			{
 				_putchar('0' + rep / 100);
 				_putchar('0' + (rep - 100) / 10);
 				_putchar('0' + rep % 10);
@@ -40,3 +46,4 @@ void print_times_table(int n)
 		_putchar('\n');
 		a++;
 	}
+}
