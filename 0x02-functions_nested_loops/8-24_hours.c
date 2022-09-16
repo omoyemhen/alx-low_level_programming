34a388a00238d3647a70263e1438f08f5f07fb5f
Omoyemhen <jessicasamuel.om@gmail.com>
2022-09-15 18:17:15 -0700

    nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,21 +14,17 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 23; a++)
 	{
-		_putchar (a / 10 + '0');
-		_putchar (a % 10 + '0');
-		_putchar (':');
-		_putchar (b / 10 + '0');
-		_putchar (b % 10 + '0');
-		_putchar ('\n');
+		for (b = 0; b <= 59; b++)
+		{
+			_putchar (a / 10 + '0');
+			_putchar (a % 10 + '0');
+			_putchar (':');
+			_putchar (b / 10 + '0');
+			_putchar (b % 10 + '0');
+			_putchar ('\n');
+
+		}
+
 	}
+
 }
-
-			
-
-			
-
-
-
-
-
-
