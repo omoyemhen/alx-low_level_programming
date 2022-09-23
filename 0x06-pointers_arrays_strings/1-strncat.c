ed2e40d7640b1ff7e769d5936bf808c5884f6e94
Omoyemhen <jessicasamuel.om@gmail.com>
2022-09-22 17:45:23 -0700

    arrays and pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -19,8 +19,8 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
-		dest[i + j] = stc[j];
+		dest[i + j] = src[j];
 	}
-		dest[i + j] = '\0';
-		return (dest);
+	dest[i + j] = '\0';
+	return (dest);
 }
