14c4159e3ff10b505b11326bb96c173a2a2b2fe2
Omoyemhen <jessicasamuel.om@gmail.com>
2022-09-26 15:49:46 -0700

    pointers and arrays

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,7 +4,8 @@
  * print_chessboard - print the chessboard, 2d array
  * @a: 2d array of chars
  * Return: void
- */
+ **/
+
 void print_chessboard(char (*a)[8])
 {
 	/*Declaring variables*/
@@ -24,3 +25,28 @@ void print_chessboard(char (*a)[8])
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
